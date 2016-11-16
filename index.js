5b08fdae33041f5de5aef5cff00f5bd9021ac9dc
JM Versteeg <janmerijnversteeg@gmail.com>
2016-11-16 22:55:36 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+module.exports               = require('./lib/ShipmentClient');
+module.exports.RunningAction = require('./lib/RunningAction');
+module.exports.RemoteError   = require('./lib/RemoteError');
